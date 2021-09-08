e7858cf804a18517c58db9edd9bc20e0dd2fa446
Robin <team@invitrics.com>
2021-09-08 02:06:24 -0500

    adding gmail authentication

--- staweb/settings.py
+++ staweb/settings.py
@@ -148,4 +148,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'devalphonsa@gmail.com'
-EMAIL_HOST_PASSWORD = '@Test123'
+EMAIL_HOST_PASSWORD = 'qwpsvuytuxyxanqz'
