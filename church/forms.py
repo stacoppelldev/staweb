2ee04ee8db5638ed191b93dad7fb0211e2d88abc
Harikrishnan A S <harikrishnan.as@qleapbs.com>
2023-09-30 18:02:28 +0530

    Provided additional spacing to identify sections logically

--- church/forms.py
+++ church/forms.py
@@ -61,6 +61,7 @@ class addFAQ(forms.ModelForm):
             'answer': forms.TextInput(attrs={'class': 'form-control'}),
         }
 
+
 class updateEventForm(forms.ModelForm):
     class Meta:
         model = event
@@ -129,6 +130,7 @@ class updateFAQForm(forms.ModelForm):
             'question': forms.TextInput(attrs={'class': 'form-control'}),
             'answer': forms.TextInput(attrs={'class': 'form-control'}),
         }
+
 
 class contactForm(forms.Form):
     name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
